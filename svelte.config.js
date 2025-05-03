f5472d8dc0fdc4b6b1461c97e9131935015e5921
Guruprasad Hegde <gruuprasada@gmail.com>
2025-05-03 19:35:26 +0530

    [deploy] Use hardcoded absolute path for assets

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -18,7 +13,7 @@ const config = {
     }),
     paths: {
       base: process.env.NODE_ENV === 'production' ? '/guhe.life' : '',
-      assets: process.env.NODE_ENV === 'production' ? resolve(__dirname, 'build') : ''
+      assets: process.env.NODE_ENV === 'production' ? '/home/gp/portfolio/guhe.life/build' : ''
     },
     prerender: {
       entries: ['*'],
