e64240f00f51958edae8318eaa0aced2b3e68d58
Guruprasad Hegde <gruuprasada@gmail.com>
2025-05-03 19:22:01 +0530

    [deploy] Remove redundant assets path configuration

--- svelte.config.js
+++ svelte.config.js
@@ -12,8 +12,7 @@ const config = {
       strict: true
     }),
     paths: {
-      base: process.env.NODE_ENV === 'production' ? '/guhe.life' : '',
-      assets: process.env.NODE_ENV === 'production' ? '/home/gp/portfolio/guhe.life/build' : ''
+      base: process.env.NODE_ENV === 'production' ? '/guhe.life' : ''
     },
     prerender: {
       entries: ['*'],
